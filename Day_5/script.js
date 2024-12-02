c87802096529622fb3617b4fd577caf08ba1019f
GauravSharma1541070 <121995754+GauravSharma1541070@users.noreply.github.com>
2024-12-02 15:46:10 +0530

    Add files via upload
    add folder day-5

---
+++ Day_5/script.js
@@ -0,0 +1,40 @@
+// function display(){
+//     console.log("hello")
+// }
+// display()
+ // let $ = 1
+
+/* function validate(){
+    let name1 = document.getElementById("name").value;
+    let pass = document.getElementById("pass").value;
+    if(name1 == "admin" && pass == "admin")
+        document.getElementById("res").innerHTML = "Login Success";
+    else
+    document.getElementById("res").innerHTML = "Login Failed";
+}
+    */
+   // let num = 12;
+   // console.log(num);
+   // let a = 23;
+   // let b = '23';
+   // console.log(a==b);
+   // console.log(a===b);
+   /* let a = (x,y) =>{
+    return x + y;
+   }
+   let x = a(12,23);
+   console.log(x)
+   */
+   /* let sayHello = () =>{
+    console.log("Hellow")
+   }
+   sayHello()
+   */
+  function display(){
+    let p1 = document.querySelector("p");
+    p1.innerHTML = "sef"
+    
+  }
+
+
+
